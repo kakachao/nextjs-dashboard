fc00b32372a5cf1a9ef7cac60db90b8dcf95202c
chc99 <2569871136@qq.com>
2023-12-21 16:23:00 +0800

    Link组件href类型修改

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -8,6 +8,7 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -25,11 +26,14 @@ export default function NavLinks() {
   return (
     <>
       {links.map((link) => {
+        console.log(link);
         const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
-            href={link.href}
+            href={{
+              pathname: link.href,
+            }}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {

a97a4a3ce3e8f5ca423e92dbaaf07c260e8ed57c
chc99 <2569871136@qq.com>
2023-12-21 17:23:17 +0800

    edit mdx-page-remote

--- app/dashboard/mdx-page-remote/page.tsx
+++ app/dashboard/mdx-page-remote/page.tsx
@@ -1,8 +1,11 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
+// import { MDXRemote } from 'next-mdx-remote/rsc'
  
-export default async function RemoteMdxPage() {
-  // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  const res = await fetch('https://...')
-  const markdown = await res.text()
-  return <MDXRemote source={markdown} />
+// export default async function RemoteMdxPage() {
+//   MDX text - can be from a local file, database, CMS, fetch, anywhere...
+//   const res = await fetch('https://...')
+//   const markdown = await res.text()
+//   return <MDXRemote source={markdown} />
+// }
+export default function Page() {
+  return <div>mdx-page-remote</div>
 }
